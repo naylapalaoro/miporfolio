0b63437248205a2bf437f7e251e8bc97b1800b52
naylapalaoro <143802416+naylapalaoro@users.noreply.github.com>
2025-02-09 11:41:13 -0300

    Add files via upload
    commit inicial

---
+++ mi-porfolio/js/proyects.js
@@ -0,0 +1,39 @@
+export const proyects = [
+    {
+        id: 1,
+        nombre : "Enigma",
+        typo : "Aplicacion web",
+        tegnologias : "React, Nodejs, bootstrap",
+        baseDatos : "MongoDB",
+        url : "https://enigma-o4tx.onrender.com",
+        descripcion : "Esta aplicacion cifra y desifra mensajes, utilizando dos tipos de cifrado, los cuales se pueden enviar por medio de distintas plataformas.",
+    },
+    { 
+        id: 2,
+        nombre: "Telemedicina",
+        typo : "Aplicacion web",
+        tegnologias : "React, Nodejs, Express, MaterialUI",
+        baseDatos : "postgreSQL",
+        url : "https://github.com/No-Country-simulation/c22-14-m-webapp",
+        descripcion : "Aplicación de telemedicina que permite a los pacientes programar citas virtuales, a los médicos acceder a las historias clínicas y a los administradores gestionar las consultas y asignar médicos, con videollamadas seguras y una gestión eficiente.",
+    },
+    {
+        id: 3,
+        nombre: "Little Books",
+        typo: "Ecommerce",
+        tegnologias: "React, Nodejs, .Net",
+        baseDatos: "postgreSQL",
+        url : "https://little-books-production.up.railway.app/",
+        descripcion: "Una aplicacion que muestra un catalogo de libros, los cuales se pueden buscar segun genero, autor o nombre del mismo, cuenta con un carrito, el cual se muestran los libros seleccionados y permite eliminarlos o agregar mas unidades, y el envio de la lista mas los datos del comprador al propietario.",
+    },
+    {
+        id: 4,
+        nombre: "Tateti",
+        typo: "Juego simple",
+        tegnologias: "html, css, javascript",
+        baseDatos: "No requiere",
+        url : "https://naylapalaoro.github.io/Tateti/",
+        descripcion: "un juego que todos conocemos cuenta con reacciones y cambios de estilos segun el ganador del juego",
+    }
+
+]
---
+++ mi-porfolio/js/scripts.js
@@ -0,0 +1,19 @@
+const toggleTheme = document.getElementById("toggle-theme");
+const toggleThemeIcon = document.getElementById("toggle-theme-icon");
+const toggleThemeText = document.getElementById("toggle-theme-text");
+const buton = document.getElementById("button-my-proyects");
+
+// Cambiar tema (oscuro/claro)
+toggleTheme.addEventListener("click", () => {
+  document.body.classList.toggle("ligth");
+  if (toggleThemeIcon.src.includes("sun.svg")) {
+    toggleThemeIcon.src = "/assets/icons/moon.svg";
+    toggleThemeText.textContent = "Modo oscuro";
+  } else {
+    toggleThemeIcon.src = "/assets/icons/sun.svg";
+    toggleThemeText.textContent = "Modo claro";
+  }
+});
+buton.addEventListener("click", () => {
+    window.location.href = "/my-proyects.html";
+});
